fbb57ec1638bbfd985e77d7ec4d0352df815625e
Joey <joey.coakley@gmail.com>
2017-04-29 15:46:49 +0000

    WHAT\'S IN THE BOX\!\? \-Brad Pitt

--- create_box/main.py
+++ create_box/main.py
@@ -1,9 +1,14 @@
 """This is the entry point of the program."""
 
-
 def create_box(height, width, character):
-    pass
+  my_string = ""
+  for i in range(height):
+    my_string = my_string + (character * width) + "\n"
+  return my_string
 
+		    
 
 if __name__ == '__main__':
     create_box(3, 4, '*')
+        
+        
